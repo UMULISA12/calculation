35f79a0340cf5bac462505787f687d56c2c39f89
UMULISA12 <email@example.com>
2018-12-12 14:03:15 +0200

    real calculator

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,19 @@ var add = function(number1 ,number2){
     };
     var number1 = parseInt(prompt("Enter a number:"));
     var number2 =parseInt(prompt("Enter a number:"));
-    alert(add(number1, number2));
+    alert("sum of the entered numbers is " +add(number1, number2));
     
-    var subtract = function(number1, number2) {
+    var substract = function(number1, number2) {
     return number1 - number2;
     };
-    alert(subtract(10,5));
+    alert ("substraction of two entered numbers is " +substract(number1,number2));
+
+    var multiply = function(number1, number2) {
+        return number1 * number2;
+        };
+        alert("the product of two entered numbers is " +multiply(number1,number2));
+
+    var divide = function(number1, number2) {
+        return number1 / number2;
+        };
+        alert("division of two entered numbers is " +divide(number1,number2));
